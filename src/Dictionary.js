7bb99edea14bacafc023a259c4e330c577d54341
Veronica <vveronikanika@gmail.com>
2022-08-18 21:43:07 +0300

    fetch: api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,18 @@
+import axios from "axios";
 import React, {useState} from "react";
 
 export default function Dictionary(){
 	let [keyword, setKeyword] = useState("");
 
+	function handleResponse(response){
+		console.log(response.data);
+	}
+
 	function search(event){
 		event.preventDefault();
 		alert(`Searching for ${keyword}`);
+		const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+		axios.get(apiUrl).then(handleResponse);
 	}
 
 	function handleSubmit(event){
