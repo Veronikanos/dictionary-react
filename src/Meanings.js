438245b8bb80f2cbdccc280e631bf6547a538fb6
Veronica <vveronikanika@gmail.com>
2022-08-21 11:40:31 +0300

    fetch: show synonyms

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meanings(props) {
 	return (
@@ -10,9 +11,11 @@ export default function Meanings(props) {
 					<div key={index}>
 						<p>{definition.definition}</p>
 						<p><em>{definition.example}</em></p>
+						<Synonyms synonyms={definition.synonyms} />
 					</div>
 				);
 			})}
 		</div>
+
 	);
 }
---
+++ src/Synonyms.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Synonyms(props){
+	if (props.synonyms) {
+		return (
+			<div className="Synonyms text-bg-danger">
+				{props.synonyms.map(function(synonym, index) {
+					return (
+					<div key={index}>
+						`Synonym is {synonym}`
+					</div>
+					)
+				})}
+			</div>
+		);
+	} else {
+		return null;
+	}
+}
