bc77f62dbe01a4393d86f6c246af073e92c41572
Veronica <vveronikanika@gmail.com>
2022-08-29 00:19:51 +0300

    fetch: add footer description

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ export default function App() {
 				<header className="App-header">Dictionary</header>
 				<Dictionary />
 			</section>
+			<footer className='text-center'>
+				This project was coded by Veronika Tlostiuk and is <a href="https://github.com/Veronikanos/dictionary-react">open-sourced on GitHub</a>
+			</footer>
     </div>
   );
 }
