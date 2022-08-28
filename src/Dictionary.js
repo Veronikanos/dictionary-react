4d9a6dda84e81c3effa5c8c8b6fbfb9e4b0f3566
Veronica <vveronikanika@gmail.com>
2022-08-28 23:59:42 +0300

    fetch: pexels photo API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,23 +1,35 @@
 import axios from "axios";
 import React, {useState} from "react";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(){
 	let [keyword, setKeyword] = useState("");
 	let [response, setResponse] = useState(null);
+	let [photos, setPhotos] = useState(null);
 
-	function handleResponse(response){
+	function handleDictionaryResponse(response){
 		setResponse(response.data[0]);
+	}
+
+	function handlePexelsResponse(response) {
+		console.log(response);
+		setPhotos(response.data.photos);
 	}
 
 	function search(event){
 		event.preventDefault();
 		const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-		axios.get(apiUrl).then(handleResponse);
+		axios.get(apiUrl).then(handleDictionaryResponse);
 		axios.get(apiUrl).catch(function (error) {
 			alert(`Sorry, I don't know word "${keyword}". Try another one :)`);
 		});
+
+		const pexelsApiKey = "563492ad6f91700001000001ccca6060857e44a185e239db2c405c8e";
+		let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+		let headers = { "Authorization" : `Bearer ${pexelsApiKey}` };
+		axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
 	}
 
 	function handleSubmit(event){
@@ -25,7 +37,7 @@ export default function Dictionary(){
 	}
 
 	return (
-		<div className="Dictionary col-11 col-md-6 mx-auto">
+		<div className="Dictionary col-11 col-md-7 mx-auto">
 			<section>
 				<form onSubmit={search} >
 					<input type="search" className="form-control" autoFocus={true} onChange={handleSubmit} placeholder="Enter your word here..."/>
@@ -34,7 +46,9 @@ export default function Dictionary(){
 					suggested word: sunrise, love, inspiration, tea...
 				</div>
 			</section>
+			<Photos photos={photos}/>
 				<Results result={response}/>
+				{/* <Photos photos={photos}/> */}
 		</div>
 	);
 }
